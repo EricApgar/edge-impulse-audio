ec405f294f9008a6a52e5d6e3e334921091616e6
EricApgar <ericapgar1@gmail.com>
2024-05-26 16:19:53 -0500

    Updated USB control.

--- control_usb.py
+++ control_usb.py
@@ -1,21 +1,29 @@
 import subprocess
 
 
-def control_usb_power(bus, port, state):
-    """
+def control_usb_power(bus, state: str):
+    '''
     Control the power of a USB port.
 
     :param bus: The USB bus (e.g., '1-1').
     :param port: The USB port number (e.g., '1').
     :param state: The desired power state ('on' or 'off').
-    """
-    command = f"sudo uhubctl -l {bus} -p {port} -a {'on' if state == 'on' else 'off'}"
+
+    Note: On the Raspberry Pi 4B, all the USB ports are linked, and so the power status of
+    all the ports is turned on or off together. So the "port" argument is not needed (or 
+    doesn't affect anything).
+    '''
+
+    # command = f"sudo uhubctl -l {bus} -p {port} -a {'on' if state == 'on' else 'off'}"
+    command = f"sudo uhubctl -l {bus} -a {'on' if state == 'on' else 'off'}"
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
     if result.returncode == 0:
-        print(f"USB port {port} on bus {bus} is now {'on' if state == 'on' else 'off'}.")
+        print(f"USB ports now {'on' if state == 'on' else 'off'}.")
     else:
-        print(f"Failed to set USB port {port} on bus {bus} to {'on' if state == 'on' else 'off'}.")
+        print(f"Failed to set USB ports to {'on' if state == 'on' else 'off'}.")
         print(result.stderr)
+
+    return
 
 
 # # Example usage
